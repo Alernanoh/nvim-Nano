0410fda352b0328fa546c35ab88e4303ba9a606a
Nano <alhernano@gmail.com>
2025-03-20 12:01:03 -0500

    Added some changes to init.lua

--- init.lua
+++ init.lua
@@ -663,10 +663,11 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         -- rust_analyzer = {},
+        --java_language_server = {},
         jdtls = {},
-        java_language_server = {},
         jsonls = {},
         ltex = {},
+        html = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
