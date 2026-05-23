d4b78cb23dd1c72696f0fc4451c7cc3d0961d57b
Nano <alhernano@gmail.com>
2026-05-23 00:35:13 -0500

    Fixed blink.cmp needing blink.lib, and added keybinds for it

--- init.lua
+++ init.lua
@@ -50,7 +50,16 @@ vim.pack.add {
 -- Adding pluggins setups
 require('blink.cmp').setup {
   keymap = {
-    preset = 'enter',
+    ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+    ['<C-e>'] = { 'hide', 'fallback' },
+    ['<CR>'] = { 'accept', 'fallback' },
+    ['<Tab>'] = { 'select_next', 'fallback_to_mappings' },
+    ['<S-Tab>'] = { 'select_prev', 'fallback_to_mappings' },
+    ['<C-p>'] = { 'snippet_forward', 'fallback' },
+    ['<C-n>'] = { 'snippet_backward', 'fallback' },
+    ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+    ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+    ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
   },
   appearance = {
     nerd_font_variant = 'mono',
